bd677a3c516a8f36c31f74bebfbd4e0daa8fa10b
user <user@users-iMac.local>
2019-12-12 11:29:02 +0300

    StarWars

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+} from 'react-native';
+
+const App = () => {
+
+    return (
+        <View style={styles.container}>
+            <Text>test star wars app</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {}
+});
+
+export default App;
