a104d98cd075c60ce54e593a0d3841f19a5f15d7
voovka <eclipse23_94@mail.ru>
2020-01-03 18:50:12 +0300

    refactor textInputs

--- src/screens/ConverterScreen/index.tsx
+++ src/screens/ConverterScreen/index.tsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react'
-import {View, Text, FlatList, ListRenderItem, TextInput, Picker} from 'react-native'
+import {View, Text, TextInput, Picker, Alert} from 'react-native'
 import {useSelector} from "react-redux";
 import {getIsDarkMode} from "@root/selectors";
 import {themeType} from "@root/redux/reducers/settingsReducer";
 import {DARK_THEME, PRIMARY_THEME} from "@root/consts/themes";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const ConverterScreen = () => {
 
@@ -11,77 +12,78 @@ export const ConverterScreen = () => {
   const theme: themeType = isDarkMode ? DARK_THEME : PRIMARY_THEME;
   const [textColor, bgColor, primary] = [theme.ON_BACKGROUND, theme.BACKGROUND, theme.PRIMARY];
 
-  type currType = { curr_id: number, curr_code: string, value: number, curr_rate: number };
-  type RenderItem = ListRenderItem<currType>;
-  type ConverterRowPropsType = { item: currType, changeSmth: (code: string, value: string) => void };
+  type currType = { curr_id: number, curr_code: string, value: string, curr_rate: number, currIconName?: string };
 
   const [currArr, setCurrArr] = useState<currType[]>([
-    {curr_id: 1, curr_code: 'BYN', value: 2.1, curr_rate: 2.1},
-    {curr_id: 2, curr_code: 'USD', value: 1, curr_rate: 1},
-    {curr_id: 3, curr_code: 'EUR', value: 0.89, curr_rate: 0.89},
-    {curr_id: 4, curr_code: 'RUB', value: 61.69, curr_rate: 61.69}
+    {curr_id: 1, curr_code: 'BYN', value: '2.11', curr_rate: 2.11},
+    {curr_id: 2, curr_code: 'USD', value: '1', curr_rate: 1, currIconName: 'usd'},
+    {curr_id: 3, curr_code: 'EUR', value: '0.89', curr_rate: 0.89, currIconName: 'eur'},
+    {curr_id: 4, curr_code: 'RUB', value: '62.09', curr_rate: 62.09}
   ]);
 
-  const changeSmth = (code: string, value: string) => {
-    console.log('code', code);
-    console.log('value', value);
+  const onChangeFieldText = (code: string, value: string) => {
+    console.log(`code: ${code} value: ${value}`);
+    if (!/^\d*\.?\d*$/.test(value)) {
+      return Alert.alert(`Invalid input`, `on ${code} currency field`);
+    }
+
+    const rate = value !== "" ? Number.parseFloat(value) / currArr.find(e => e.curr_code === code)!.curr_rate : 0;
+    console.log('rate', rate);
+
     let tmp = [...currArr].map((e) => {
-      if (e.curr_code === code) e.value = Number.parseFloat(value);
+      e.value = e.curr_code === code ? value : (Math.round(rate * e.curr_rate * 100) / 100).toString();
       return e;
     });
     setCurrArr(tmp);
   };
 
-  const ConverterRow = (props: ConverterRowPropsType) => {
-    const {item, changeSmth} = props;
-    const [value, setValue] = useState<string>('');
-    const [currVal, setCurrVal] = useState<string>(item.curr_code);
-
-    const onChangeText = (text: string) => {
-      console.log('changeSmth');
-      // setValue(text);
-      changeSmth(currVal, text);
-    };
-
-    return (
-      <View style={{flexDirection: 'row', paddingVertical: 5, marginVertical: 3, alignItems: 'center'}}>
-        <TextInput style={{width: 250, height: 40, borderColor: textColor, color: textColor, borderWidth: 1}}
-                   placeholder={'0'}
-                   placeholderTextColor={textColor}
-                   keyboardType={'decimal-pad'}
-                   value={item.value.toString()}
-                   autoFocus={true}
-          // value={value}
-                   onChangeText={onChangeText}
-          // onChangeText={text => {
-          //   console.log('text',text);
-          //   onChangeText(text)}}
-        />
-        <Picker selectedValue={currVal}
-                onValueChange={(itemValue) => setCurrVal(itemValue)}
-                style={{width: 50, height: 50}}
-                itemStyle={{height: 50, color: primary}}>
-          {currArr.map(e => (<Picker.Item key={e.curr_id} label={e.curr_code} value={e.curr_code}/>))}
-        </Picker>
-      </View>
-    )
+  const onChangeFieldCurrency = (nextVal: string, prevVal: string) => {
+    if (nextVal !== prevVal) {
+      console.log(`${prevVal} => ${nextVal}`);
+      const currField = currArr.find((e) => e.curr_code === prevVal);
+      if (!currField) {
+        console.log('currField', currField);
+        onChangeFieldText(nextVal, currField!.value);
+      } else Alert.alert('Error','Your currency already exists in the list')
+    }
   };
-
-  const renderItem: RenderItem = ({item}) => {
-    return <ConverterRow item={item} changeSmth={changeSmth}/>
-  };
-
-  const keyExtractor = (item: currType) => item.curr_id.toString();
 
   return (
-    <View style={{flex: 1, backgroundColor: bgColor, alignItems: 'center'}}>
+    <View style={{flex: 1, backgroundColor: bgColor, alignItems: 'center', justifyContent: 'flex-start'}}>
       <Text style={{color: textColor, paddingVertical: 20, fontSize: 20}}>Converter screen</Text>
-
-      {/*{currArr.map(e => <ConverterRow key={e.curr_id} item={e} changeSmth={changeSmth}/>)}*/}
-      <FlatList data={currArr}
-                renderItem={renderItem}
-                keyExtractor={keyExtractor}
-                style={{}}/>
+      {
+        currArr.map(e => {
+            return (
+              <View key={e.curr_id}
+                    style={{flexDirection: 'row', marginVertical: 3, alignItems: 'center'}}>
+                <View style={{width: 25, alignItems: 'center'}}>
+                  {e.currIconName && <Icon name={e.currIconName} size={20} color={textColor}/>}
+                </View>
+                <TextInput style={{
+                  width: 250,
+                  height: 40,
+                  borderColor: textColor,
+                  color: textColor,
+                  borderWidth: 1,
+                  marginHorizontal: 3
+                }}
+                           placeholder={'0'}
+                           placeholderTextColor={textColor}
+                           keyboardType={'decimal-pad'}
+                           value={e.value}
+                           onChangeText={(text) => onChangeFieldText(e.curr_code, text)}
+                />
+                <Picker selectedValue={e.curr_code}
+                        onValueChange={(itemValue) => onChangeFieldCurrency(itemValue, e.curr_code)}
+                        style={{width: 50, height: 50}}
+                        itemStyle={{height: 50, color: primary}}>
+                  {currArr.map(e => (<Picker.Item key={e.curr_id} label={e.curr_code} value={e.curr_code}/>))}
+                </Picker>
+              </View>
+            )
+          }
+        )
+      }
     </View>
   )
 };
